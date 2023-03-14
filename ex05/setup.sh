633c56de0dc4c14917906cd0949de2ea88c11670
Louis Losson <louis.losson@epitech.eu>
2023-03-14 15:31:31 +0100

    [ADD]: script for downloading the dataset

---
+++ ex05/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# variables
+FILE="$PWD/dataset/dataset.csv"
+LINK="https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv"
+ANSWER="n"
+
+
+echo -e """This script will download the dataset into $PWD/dataset/
+Are you sure you want to continue? [y/n] (default: n)
+"""
+
+read ANSWER
+if [ "$ANSWER" = "y" ]; then
+    echo "Downloading dataset..."
+    wget -O dataset/dataset.csv $LINK
+    echo "Done."
+else
+    echo "Exiting..."
+    exit 0
+fi
