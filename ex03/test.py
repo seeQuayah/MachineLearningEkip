db5b2f6c1b8a2178617f133384f24302a4f15d12
seeQuayah <jean-christophe.della-giusta@epitech.eu>
2023-03-19 12:05:00 +0100

    commit

--- ex03/test.py
+++ ex03/test.py
@@ -1,39 +1,51 @@
 import numpy as np
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+from scipy.spatial.distance import cdist
+from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
+from yellowbrick.cluster import KElbowVisualizer
 
 # Chargement des données
 data = np.load('data.npy')
 
-inertias = []
-for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(data)
-    inertias.append(kmeans.inertia_)
+def kmeans_euclidian_inertia():
+    inertias = []
+    for k in range(1, 11):
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(data)
+        inertias.append(kmeans.inertia_)
+    return inertias
 
-sil_scores = []
-for k in range(2, 11):
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(data)
-    sil_score = silhouette_score(data, kmeans.labels_)
-    sil_scores.append(sil_score)
+inertias = kmeans_euclidian_inertia()
 
+def kmeans_euclidian_sil_scores():
+    sil_scores = []
+    for k in range(2, 11):
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(data)
+        sil_score = silhouette_score(data, kmeans.labels_)
+        sil_scores.append(sil_score)
+    return sil_scores
+sil_scores = kmeans_euclidian_sil_scores()
 scaler = MinMaxScaler()
 data_scaled = scaler.fit_transform(data)
 
-calinski_scores = []
-for i in range(2, 11):
-    agglo = AgglomerativeClustering(n_clusters=i, affinity='manhattan', linkage='average')
-    agglo.fit(data_scaled)
-    calinski_scores.append(calinski_harabasz_score(data_scaled, agglo.labels_))
 
-davies_scores = []
-for i in range(2, 11):
-    agglo = AgglomerativeClustering(n_clusters=i, affinity='manhattan', linkage='average')
+def elbow_agglo():
+    agglo = AgglomerativeClustering(metric='manhattan', linkage='average')
     agglo.fit(data_scaled)
-    davies_scores.append(davies_bouldin_score(data_scaled, agglo.labels_))
+    results = KElbowVisualizer(agglo, k=(2,12))
+    results.fit(data)
+    results.show()
+
+elbow_agglo()
+
+silhou = []
+for i in range(2, 11):
+    agglo = AgglomerativeClustering(n_clusters=i, metric='manhattan', linkage='average')
+    agglo.fit(data_scaled)
+    silhou.append(silhouette_score(data_scaled, agglo.labels_))
 
 
 figure, axis = plt.subplots(2, 2, figsize=(10, 10))
@@ -57,9 +69,9 @@ axis[1, 0].set_xlabel('Nombre de clusters')
 axis[1, 0].set_ylabel('Score de Calinski-Harabasz')
 
 # AgglomerativeClustering with Davies-Bouldin index
-axis[1, 1].plot(range(2, 11), davies_scores)
-axis[1, 1].set_title('INdice de Davies-Bouldin (Agglomerative clustering)')
+axis[1, 1].plot(range(2, 11), silhou)
+axis[1, 1].set_title('silouhette (Agglomerative clustering)')
 axis[1, 1].set_xlabel('Nombre de clusters')
-axis[1, 1].set_ylabel('Score de Davies-Bouldin')
+axis[1, 1].set_ylabel('Score')
 
 plt.show()
